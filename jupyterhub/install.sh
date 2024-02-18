01877029f54a5353a78b2c98de4f78706e68f40f
Mikaël Dusenne (London) <mikaeldusenne@gmail.com>
2024-02-18 14:14:48 +0000

    fix jupyterhub

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -8,4 +8,7 @@ echo 'setting up jupyterhub...'
 # prepare jupyterhub docker spawning instances
 cd jupyterhub/jupyter && docker build . -t $JUPYTERHUB_JUPYTER_IMAGE_NAME
 
+mkdir -p jupyterhub/data
+cp -i jupyterhub/jupyterhub_config.py jupyterhub/data
+
 echo 'Done setting up jupyterhub.'
