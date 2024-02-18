820c179eec6034572716158eab2683b98964f006
Mikaël Dusenne (London) <mikaeldusenne@gmail.com>
2024-02-18 17:05:59 +0000

    matrix logging fix

--- matrix/install.sh
+++ matrix/install.sh
@@ -9,13 +9,14 @@ source .env
 
 mkdir -p ./matrix/matrix/data/
 touch ./matrix/matrix/data/homeserver.yaml # to have write rights
+touch ./matrix/matrix/data/eds-matrix.log.config # to have write rights
 
 sudo chown 991:991 ./matrix/matrix/data
 
 envsubst < matrix/matrix/homeserver.yaml > matrix/matrix/data/homeserver.yaml
+cp matrix/matrix/eds-matrix.log.config matrix/matrix/data/eds-matrix.log.config 
 
-# sed -i 's;/homeserver.log;/data/homeserver.log;' matrix/matrix/data/eds-matrix.log.config && cat matrix/matrix/data/eds-matrix.log.config | grep homeserver.log
-
+# sed 's;/homeserver.log;/data/homeserver.log;' matrix/matrix/eds-matrix.log.config && cat matrix/matrix/data/eds-matrix.log.config | grep homeserver.log
 envsubst < matrix/element/element-config_template.json > matrix/element/element-config.json
 
 echo 'Done setting up matrix.'
