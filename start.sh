f40f81273061d21f72c472f88fe70a3d944f07c0
Mikaël Dusenne (London) <mikaeldusenne@gmail.com>
2024-02-18 13:43:59 +0000

    add start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker compose \
+       -f compose_network.yml \
+       -f compose_nginx.yml \
+       -f compose_keycloak.yml \
+       -f compose_jupyter.yml \
+       -f compose_matrix.yml \
+       up --abort-on-container-exit --build
