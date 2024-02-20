7d316d9936b00d63af04c9c178ecf04cd8c6eacc
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-20 13:51:44 +0100

    matrix fix

--- matrix/install.sh
+++ matrix/install.sh
@@ -6,15 +6,16 @@ set -u
 echo 'setting up matrix...'
 
 source .env
+EDS_CONTAINER_PREFIX=${EDS_CONTAINER_PREFIX:-eds}
 
 mkdir -p ./matrix/matrix/data/
 touch ./matrix/matrix/data/homeserver.yaml # to have write rights
-touch ./matrix/matrix/data/eds-matrix.log.config # to have write rights
+touch matrix/matrix/data/$EDS_CONTAINER_PREFIX-matrix.log.config # to have write rights
 
 sudo chown 991:991 ./matrix/matrix/data
 
 envsubst < matrix/matrix/homeserver.yaml > matrix/matrix/data/homeserver.yaml
-cp matrix/matrix/eds-matrix.log.config matrix/matrix/data/eds-matrix.log.config 
+cp matrix/matrix/matrix.log.config matrix/matrix/data/$EDS_CONTAINER_PREFIX-matrix.log.config 
 
 # sed 's;/homeserver.log;/data/homeserver.log;' matrix/matrix/eds-matrix.log.config && cat matrix/matrix/data/eds-matrix.log.config | grep homeserver.log
 envsubst < matrix/element/element-config_template.json > matrix/element/element-config.json
