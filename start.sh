f3e880c6f4bdc6bfc17765c04a1dac72e3b492cb
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-21 18:44:48 +0100

    start.sh automatically recreate the docker network, automatically runs docker compose down after each up, hopefully to stop the nightmare of compose inconsistencies !

--- start.sh
+++ start.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-docker compose \
-       -f compose_network.yml \
-       -f compose_nginx.yml \
-       -f compose_keycloak.yml \
-       -f compose_jupyter.yml \
-       -f compose_matrix.yml \
-       -f compose_gitlab.yml \
-       up --abort-on-container-exit --build
 
-if [ "$1" = "1" ]; then
-    sudo ./lib/set_hosts
+# if [ -z "$1" ]; then
+#     args='up --abort-on-container-exit --build'
+# else
+#     args=$@
+# fi
+
+yamls=''
+action='up --abort-on-container-exit --build'
+
+while [[ $# > 0 ]];do
+	case "$1" in
+        --action)
+            
+            action=$2;
+            shift ;;
+        *) yamls="$yamls -f compose_${1}.yml" ;;
+	esac
+	shift
+done
+
+if [ -z "$yamls" ]; then
+    yamls="-f compose_network.yml -f compose_nginx.yml -f compose_keycloak.yml -f compose_jupyter.yml -f compose_matrix.yml -f compose_gitlab.yml"
 fi
+
+
+echo $action | grep -q "\bup\b" && ./lib/eds_recreate_network
+echo $action | grep -q "\bup\b" && (sleep 10 && sudo ./lib/set_hosts) &
+
+echo "docker compose $yamls $action"
+docker compose $yamls $action
+
+echo $action | grep -q "\bup\b" && docker compose $yamls down
+
