92a00b07203621a9e8b89f19fb7e4db968e769e2
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-23 21:07:36 +0100

    nginx jitsy

--- nginx/install.sh
+++ nginx/install.sh
@@ -13,7 +13,7 @@ if [ -n "$SERVER_CERTS" ]; then
     cp "$SERVER_CERTS/fullchain.pem" nginx/certificates/fullchain.pem
 else
     # ./lib/create_certificates.sh --name nginx
-    ./lib/create_certificates.sh --name nginx --san "DNS:keycloak.$EDS_DOMAIN, DNS:gitlab.$EDS_DOMAIN, DNS:matrix.$EDS_DOMAIN, DNS:jupyter.$EDS_DOMAIN, DNS:$EDS_DOMAIN"
+    ./lib/create_certificates.sh --name nginx --san "DNS:keycloak.$EDS_DOMAIN, DNS:gitlab.$EDS_DOMAIN, DNS:matrix.$EDS_DOMAIN, DNS:jupyter.$EDS_DOMAIN, DNS:meet.$EDS_DOMAIN, DNS:$EDS_DOMAIN"
     cp "./nginx/certificates/nginx.key" ./nginx/certificates/privkey.pem
     cp "./nginx/certificates/nginx.crt" ./nginx/certificates/fullchain.pem
 fi
