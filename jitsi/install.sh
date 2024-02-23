41b0b1feb8a6ede16d32d5aadbaaa87458f1262b
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-23 20:55:50 +0100

    gitignore

---
+++ jitsi/install.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+set -eu
+
+source .env
+
+cd jitsi
+
+if ! [ -e "jitsi-meet-cfg" ]; then
+    
+    wget $(curl -s https://api.github.com/repos/jitsi/docker-jitsi-meet/releases/latest | grep 'zip' | cut -d\" -f4)
+
+    unzip $(ls -tr | tail -n1)
+
+    mv -i $(find . -maxdepth 1 -type d  -name '*jitsi-docker-jitsi-meet*') jitsi-docker
+    
+    cd jitsi-docker
+    
+    cp -i env.example .env
+    ./gen-passwords.sh
+    
+    mkdir -p ./jitsi-meet-cfg/{web,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+
+    cd ../
+fi
+
+cd ../
+
+envsubst < jitsi/.env.template > jitsi/jitsi-docker/.env
+
+
+
