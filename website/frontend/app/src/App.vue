d918bf336c107325ec1d8072e41882b2becd1ffd
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-26 19:28:42 +0100

    vue landing page

---
+++ website/frontend/app/src/App.vue
@@ -0,0 +1,110 @@
+
+<template>
+  <div class="services-container">
+    <ServiceIcon
+      v-for="service in services"
+      :key="service.name"
+      :name="service.name"
+      :description="service.description"
+      :icon="service.icon"
+      :link="service.link"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ServiceIcon from './components/ServiceIcon.vue';
+
+/* components: {
+ *   ServiceIcon,
+ * }, */
+const services = ref([
+  {
+    name: 'Jupyter',
+    description: 'Calcul interactif et cahiers de notes.',
+    link: 'jupyter',
+  },
+  {
+    name: 'GitLab',
+    description: 'Plateforme DevOps pour vos projets.',
+    link: 'gitlab',
+  },
+  {
+    name: 'Element',
+    description: 'Messagerie sécurisée et collaboration.',
+    link: 'chat',
+  },
+  {
+    name: 'Jitsi',
+    description: 'Conférences vidéo sécurisées et open source.',
+    link: 'jitsi',
+  },
+  {
+    name: 'Seafile',
+    description: 'Stockage et synchronisation de fichiers haute performance.',
+    link: 'seafile',
+  },
+  {
+    name: 'Draw.io',
+    description: 'Outil de diagrammes en ligne gratuit et open source.',
+    link: 'drawio',
+  },
+  {
+    name: 'OnlyOffice',
+    description: 'Suite bureautique en ligne pour la collaboration sur des documents.',
+    link: 'onlyoffice',
+  },
+])
+
+</script>
+
+<style >
+.logo {
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+body, html {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
+#app {
+  /* display: flex;
+     justify-content: center;
+     align-items: center; */
+  /* padding: 20px; */
+  width: 80%;
+  box-sizing: border-box;
+}
+.services-container {
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  justify-items: stretch;
+  align-items: top;
+
+  column-gap: 2rem;
+  row-gap: 2rem;
+  width: 100%; /* Ensures the container does not overflow the viewport */
+  /* overflow-y: auto; /* Allows scrolling on overflow */
+}
+
+@media (max-width: 768px) {
+  .services-container {
+    grid-template-columns: 50% 50%;
+  }
+}
+
+@media (max-width: 480px) {
+  .services-container {
+    grid-template-columns: 100%;
+  }
+}
+</style>
---
+++ website/frontend/app/src/components/ServiceIcon.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="service" @click="openService(href)">
+    <!-- <img :src="icon" :alt="name" class="icon"> -->
+    <img :src="iconsrc" :alt="name" class="icon">
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  name: String,
+  description: String,
+  link: String,
+})
+
+function openService(url) {
+  window.open(url, '_blank');
+}
+
+const href = computed(() => {
+  return `https://${props.link}.${import.meta.env.VITE_EDS_DOMAIN}`
+})
+
+const iconsrc = computed(() => {
+  return `/${props.link}.jpg`
+})
+
+</script>
+
+<style scoped>
+.service {
+  background: #eee;
+  padding: 1rem;
+  
+  cursor: pointer;
+  text-align: center;
+  min-width: 25%;
+  max-width: 100%;
+}
+.icon {
+  border: 2px solid #ddd;
+  max-height: 10rem;
+  width: auto;
+  aspect-ratio: 1/1 !important;
+  max-width: 100%;
+}
+.service {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+}
+
+@media (max-width: 768px) {
+  .service {
+  }
+}
+
+
+.service-info {
+  text-align: center;
+}
+
+</style>
+
