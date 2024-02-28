1e15a4da42534bcc1a42b303b2f2a3ced62c3a3d
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-02-28 11:48:44 +0100

    mongo fix init

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,21 +1,17 @@
+db = db.getSiblingDB('admin');
+
 db.createUser({
     user: "$MONGO_INITDB_ROOT_USERNAME",
     pwd: "$MONGO_INITDB_ROOT_PASSWORD",
     roles: [
         {
-            role: "userAdminAnyDatabase",
-            db: "admin",
-        },
-        {
-            role: "dbAdminAnyDatabase",
-            db: "admin",
-        },
-        {
-            role: "readWriteAnyDatabase",
-            db: "admin",
+            role: "root",
+            db: "admin"
         },
     ]
 })
+
+db = db.getSiblingDB('$MONGO_INITDB_DATABASE');
 
 db.createUser(
 	  {
